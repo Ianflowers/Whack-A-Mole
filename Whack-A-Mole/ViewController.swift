be185b8e7f81ba0a655f5cbdc3150b7af4268900
Ian Flores <mazing.pollo@gmail.com>
2020-10-06 13:43:28 -0500

    Initial Commit

---
+++ Whack-A-Mole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whack-A-Mole
+//
+//  Created by Ian Flores on 10/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
